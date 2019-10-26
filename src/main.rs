72b6de64531ef20293897702e28cb8f148f7b815
Jared Kerim <jared.l.kerim@gmail.com>
2019-10-26 01:10:56 -0400

    Sign a message with dalek

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use ed25519_dalek::Keypair;
+use ed25519_dalek::Signature;
+use rand::rngs::OsRng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut csprng: OsRng = OsRng::new().unwrap();
+    let keypair: Keypair = Keypair::generate(&mut csprng);
+
+    let message: &[u8] = b"This message is signed.";
+    let signature: Signature = keypair.sign(message);
+
+    assert!(keypair.verify(message, &signature).is_ok());
 }
