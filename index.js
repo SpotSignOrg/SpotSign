9e6b735c73f9f86be19d71901e9c28d7a9f9987e
Jared Kerim <jared.l.kerim@gmail.com>
2019-10-27 08:24:36 -0400

    Support entering info to validate

--- index.js
+++ index.js
@@ -64,13 +64,19 @@ function compress(string) {
 
 rust
     .then(m => {
-        let keys = m.get_keys();
-        console.log(keys);
-        document.getElementById("privatekey").value = keys.private_key;
-        document.getElementById("publickey").value = keys.public_key;
+
+        document.getElementById("generate").addEventListener("click", e => {
+            let keys = m.get_keys();
+            console.log(keys);
+            document.getElementById("privatekey").value = keys.private_key;
+            document.getElementById("publickey").value = keys.public_key;
+
+        })
         document.getElementById("sign").addEventListener("click", e => {
+            let private_key = document.getElementById("privatekey").value;
+            let public_key = document.getElementById("publickey").value;
             let message = document.getElementById("message").value;
-            let signature = m.sign_message(keys.private_key, keys.public_key, message);
+            let signature = m.sign_message(private_key, public_key, message);
             console.log(signature);
             let compressed_signature = compress(signature.signature);
             document.getElementById("signature").value = compressed_signature;
@@ -78,8 +84,9 @@ rust
         })
         document.getElementById("verify").addEventListener("click", e => {
             let message = document.getElementById("message").value;
+            let public_key = document.getElementById("publickey").value;
             let signature = decompress(document.getElementById("signature").value);
-            let verification = m.verify_message(keys.public_key, signature, message);
+            let verification = m.verify_message(public_key, signature, message);
             document.getElementById("verification").innerHTML = verification.verified;
         })
     })
