f598dcd86bfda972ef0301f6011e8510908715f8
Jared Kerim <jared.l.kerim@gmail.com>
2019-10-27 03:08:13 -0400

    Add wasm_bindgen

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,23 @@
+extern crate rand;
+
 use ed25519_dalek::Keypair;
 use ed25519_dalek::Signature;
 use ed25519_dalek::PublicKey;
 use rand::rngs::OsRng;
+use wasm_bindgen::prelude::*;
+use base64::encode;
 
-fn main() {
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    console_error_panic_hook::set_once();
+
     let mut csprng: OsRng = OsRng::new().unwrap();
+
     let keypair: Keypair = Keypair::generate(&mut csprng);
 
     let message: &[u8] = b"This message is signed.";
@@ -14,4 +27,9 @@ fn main() {
 
     let public_key: PublicKey = keypair.public;
     assert!(public_key.verify(message, &signature).is_ok());
+
+    let public_key_bytes = &public_key.to_bytes();
+    let public_key_string = encode(public_key_bytes);
+
+    alert(&format!("Hello, {}!", public_key_string));
 }
