9ee6b806c2aa369478ac973de5f3698a050ee709
Travel <travel@jkerim-bmjgh6.local>
2019-11-10 18:18:28 +0100

    Reorganize addon files

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -1,27 +1,40 @@
 const path = require("path");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 
 module.exports = {
   entry: {
-    background_scripts: "./background_scripts/background.js",
-    content_scripts: "./content_scripts/content.js",
-    popup: "./popup/popup.js"
+    background_scripts: "./addon/background_scripts/background.js",
+    content_scripts: "./addon/content_scripts/content.js",
+    popup: "./addon/popup/popup.js"
   },
   output: {
-    path: path.resolve(__dirname, "addon"),
+    path: path.resolve(__dirname, "dist"),
     filename: "[name]/index.js"
   },
   plugins: [
     new WasmPackPlugin({
       crateDirectory: path.resolve(__dirname, "../signer")
-    })
+    }),
+    new CopyPlugin([
+      { from: './addon/manifest.json', to: 'manifest.json' },
+      { from: './addon/icons/pen.png', to: 'icons/pen.png' },
+      { from: './addon/popup/popup.html', to: 'popup/popup.html' },
+
+    ]),
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.html$/,
+        use: [{
+          loader: 'html-loader'
+        }]
       }
     ]
   },
