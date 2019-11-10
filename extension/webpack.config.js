4c2dc4b27e5a04a6a1f77096ef311861691738ad
Travel <travel@jkerim-bmjgh6.local>
2019-11-10 02:07:58 +0100

    Get latest webpack working without using eval

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -9,5 +9,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "addon"),
         filename: "[name]/index.js"
-    }
+    },
+    mode: "development",
+    devtool: "source-map",
 };
